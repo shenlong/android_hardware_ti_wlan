c8b8a44ff3dfb19b0af1cbf5c0176e7decdf59da
Panduranga Mallireddy <panduranga_mallireddy@ti.com>
2012-10-16 14:53:26 -0500

    Updating the WLAN Calibration Script
    This make sure that it will only calibrate on R5 based driver.
    Even if we hack the module to be loaded to wlcore_sdio.ko to use WiLink8
    in init.rc and user has WiLink7, we can still use the script fo calibration
    
    If the calibartion fails it will restore the previous nvs file.
    
    Change-Id: I6e5a100ef7485ec5d408ad33f1cd430b2128c995
    Signed-off-by: Panduranga Mallireddy <panduranga_mallireddy@ti.com>

--- mac80211/config/WLAN_Calibration_Script.sh
+++ mac80211/config/WLAN_Calibration_Script.sh
@@ -1,4 +1,22 @@
 #! /system/bin/sh
+TARGET_FW_DIR=/system/etc/firmware/ti-connectivity
+TARGET_NVS_FILE=$TARGET_FW_DIR/wl1271-nvs.bin
+TARGET_INI_FILE=/system/etc/wifi/TQS_D_1.7.ini
+WL12xx_MODULE=/system/lib/modules/wl12xx_sdio.ko
+WLCORE_MODULE=/system/lib/modules/wlcore.ko
+SDIO_MODULE=/system/lib/modules/wlcore_sdio.ko
+WL7_MODULE=/system/lib/modules/wl7/wl12xx_sdio.ko
+
+if [ -e $WLCORE_MODULE ];
+then
+    echo "************************************************************"
+    echo "Using R8 driver"
+    echo "If this is omap5/wl18xx platform, Calibration is not needed"
+    echo "************************************************************"
+    exit
+else
+   echo ""
+fi
 
 DRIVER_STATE=`getprop wlan.driver.status`
 
@@ -12,27 +30,14 @@ case "$DRIVER_STATE" in
              echo " ******************************";;
 esac
 
-TARGET_FW_DIR=/system/etc/firmware/ti-connectivity
-TARGET_NVS_FILE=$TARGET_FW_DIR/wl1271-nvs.bin
-TARGET_INI_FILE=/system/etc/wifi/TQS_D_1.7.ini
-WL12xx_MODULE=/system/lib/modules/wl12xx_sdio.ko
-
-if [ -e $WL12xx_MODULE ];
-then
-    echo ""
-else
-    echo "********************************************************"
-    echo "wl12xx_sdio module not found !!"
-    echo "If this is omap5/wl18xx platform, Calibration is not needed"
-    echo "********************************************************"
-    exit
-fi
-
 # Remount system partition as rw
 mount -o remount rw /system
 
+# Point it to module in wl7 folder if it is not present in /system/lib/modules/
+[ ! -f $WL12xx_MODULE ] && WL12xx_MODULE=$WL7_MODULE;
+
 # Remove old NVS file
-rm /system/etc/firmware/ti-connectivity/wl1271-nvs.bin
+`mv $TARGET_NVS_FILE $TARGET_FW_DIR/wl1271-nvs.bin.org`
 
 # Actual calibration...
 # calibrator plt autocalibrate <dev> <module path> <ini file1> <nvs file> <mac addr>
@@ -40,5 +45,14 @@ rm /system/etc/firmware/ti-connectivity/wl1271-nvs.bin
 calibrator plt autocalibrate wlan0 $WL12xx_MODULE $TARGET_INI_FILE $TARGET_NVS_FILE
 
 echo " ******************************"
+
+if [ -f $TARGET_NVS_FILE ]; then
 echo " * Finished Wi-Fi calibration *"
+`rm $TARGET_FW_DIR/wl1271-nvs.bin.org`
+else
+echo " * Wi-Fi calibration Failed  *"
+echo " * Restored default nvs file *"
+`mv $TARGET_FW_DIR/wl1271-nvs.bin.org $TARGET_NVS_FILE`
+fi
+
 echo " ******************************"
